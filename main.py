5f2063cf244a938cd23e55e132a6e18c67d4166d
C M J Narasimha Naidu <maddunaidu18@gmail.com>
2026-07-20 16:27:23 +0530

    changes done

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy import Column,Integer,String,Boolean
+engine=create_engine("sqlite:///todo.db")
+Base=declarative_base()
+class Todo(Base):
+     __tablename__="tasks"
+     id=Column(Integer,primary_key=True,index=True)
+     title=Column(String,nullable=True)
+     completed=Column(Boolean,nullable=True)
+Base.metadata.create_all(bind=engine)
+session_local=sessionmaker(bind=engine)
+db=session_local()
 app=FastAPI()
-todo_list=[]
 class Model(BaseModel):
+    id:int
     title:str
     completed:bool
 @app.get("/")
@@ -10,36 +23,41 @@ def start():
     return "Hello world"
 @app.post("/tasks")
 def create(task:Model):
-    todo_list.append({"id":len(todo_list)+1,
-        "title":task.title,
-        "completed":task.completed})
+    task=Todo(title=task.title,completed=task.completed)
+    db.add(task)
+    db.commit()
     return f"Successfully added to to-do task {task.title}"
 @app.get("/tasks/{id}")
 def read_todo(id:int):
-        if len(todo_list)>0:
-            for todo in todo_list:
-                if todo["id"]==id:
-                    return todo
+        tasks = db.query(Todo).all()
+        if len(tasks)>0:
+            for todo in tasks:
+                if todo.id==id:
+                    return {
+                         "id":todo.id,
+                         "title":todo.title,
+                         "completed":todo.completed
+                    }
         else:
             return "Oops i could't find your todo"
 @app.put("/tasks/{id}")
-def update(id:int,task:Model):
-    for todo in todo_list:
-        if todo["id"]==id:
-            todo["title"]=task.title
-            todo["completed"]=task.completed
-            return f"Successfully updated your to-do here is the updated one {todo_list[id-1]}"
+def update(task:Model):
+    todo = db.query(Todo).filter(Todo.id ==task.id).first()
+    if todo is None:
+         return "Oops we couldn't find the todo"
     else:
-        return "Oops your todo is found please try again"
+        todo.title=task.title
+        todo.completed=task.completed
+        db.commit()
+        return f"Successfully updated your to-do here is the updated one"
 @app.delete("/tasks/{id}")
 def delete(id:int):
-    for todo in todo_list:
-        if todo["id"]==id:
-            todo_list.remove(todo)
-            return "Your todo is Successfully deleted"
+    todo=db.query(Todo).filter(Todo.id==id).first()
+    if todo is None:
+         return "Oops we couldn't find the todo"
     else:
-            return "Oops your to-do not found please try again"
-
-
+        db.delete(todo)
+        db.commit()
+        return "Successfully deleted the todo"
 
 
